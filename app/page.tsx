a20b0f155c830d85f822e5b3040f630d9f6e4129
NikhilThakur3536 <thakurnikhil657@gmail.com>
2025-04-17 12:16:55 +0530

    BlackInfoCards addded

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { BlackBgInfoCards } from "@/components/BlackBgInfoCards";
 import { HeroSection } from "@/components/HeroSection";
 import { NavBar } from "@/components/NavBar";
 import { UpgradeYourPracticeSection } from "@/components/UpgradeYourPracticeSection";
+import { BlackBgInfoCardsData } from "@/data/BlackBgInfoCardsData";
+import Image from "next/image";
 
 
   export default function Home() {
@@ -12,7 +15,31 @@ import { UpgradeYourPracticeSection } from "@/components/UpgradeYourPracticeSect
         </div>
         <section className="relative w-screen h-screen overflow-hidden px-12">
           <UpgradeYourPracticeSection/>
-        </section>    
+        </section>   
+        <section className="w-screen h-screen ">
+          <div className="w-full h-full flex flex-col items-center">
+            <div className="bg-black w-[95%] h-[50%] rounded-xl mt-10 flex justify-center py-10 px-8">
+              <div className="w-full h-full rounded-xl flex">
+                <div className="w-[15%] h-full flex flex-col ">
+                  <h3 className="text-[#005ab1] px-4">THE ORBSWAY EFFECT</h3>
+                  <h3 className=" mt-6 text-3xl  text-white px-4">
+                    Trusted by 
+                    Clinics.Proven 
+                    with patient 
+                  </h3>
+                  <p className="text-white mt-4 text-sm ml-4">
+                    From Hospitals to homes, 
+                    Orbsway is helping hundreds 
+                    improve thier vision
+                  </p>
+                </div>
+                <div className="flex justify-around w-[80%] ml-24">
+                  {BlackBgInfoCardsData.map((items,index)=>(<BlackBgInfoCards key={index} {...items}/>))}
+                </div>
+              </div>
+            </div>
+          </div>  
+        </section> 
       </>  
     );
   }
---
+++ components/BlackBgInfoCards.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import Image from "next/image"
+
+export type BlackBgInfoCardsProps={
+    number:number,
+    heading:string,
+    description:string,
+}
+
+export const BlackBgInfoCards=({number,heading,description}:BlackBgInfoCardsProps)=>{
+    return(
+        <div className="bg-white h-full w-[30%] rounded-xl flex flex-col items-center">
+            <p className="text-8xl font-bold mt-8 px-2 bg-gradient-to-r from-blue-950 via-blue-800 to-blue-500 bg-clip-text text-transparent">
+                {number}
+            </p>
+            <div className="flex mt-8">
+                <Image src={"/hospital.svg"} width={50} height={40} alt="hospital" className="w-fit-content h-fit-content p-2 bg-gray-200 rounded-xl"/>
+                <div className="flex flex-col ml-4  ">
+                <p className="font-bold">{heading}</p>
+                <p>{description}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
