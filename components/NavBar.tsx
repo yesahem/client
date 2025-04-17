c517cdb8ea07ff8390468d8a752519598c8fcce5
NikhilThakur3536 <thakurnikhil657@gmail.com>
2025-04-17 06:53:08 +0530

    Nav Bar Added

---
+++ components/NavBar.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import Image from "next/image"
+
+export const NavBar = () => {
+    return (
+        <div className="flex justify-between h-[10%] w-full">
+            <div className="w-[20%] h-full flex justify-center items-center">
+                <Image src={"/logo.png"} width={100} height={100} alt="logo"/>
+            </div> 
+            <div className="w-[20%] h-full flex justify-center items-center gap-x-4">
+                <h3 className="">Products</h3>
+                <h3 className="">Contact</h3>
+            </div> 
+        </div>
+    )
+}
