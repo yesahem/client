e895ee41f4434c4fde0c929e83889db82f176a30
NikhilThakur3536 <thakurnikhil657@gmail.com>
2025-04-20 22:10:46 +0530

    NavBar & height in a component issue fixed

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,7 +6,7 @@ export const NavBar = () => {
     return (
         <div className="flex justify-between h-[10%] w-full mb-4">
             <div className="w-[20%] h-full flex justify-center items-center">
-                <Image src={"/logo.png"} width={100} height={100} alt="logo"/>
+                <Image src={"/Logo.png"} width={100} height={100} alt="logo"/>
             </div> 
             <div className="w-[20%] h-full flex justify-center items-center gap-x-4">
                 <h3 className="">Products</h3>
